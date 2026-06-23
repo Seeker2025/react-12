c47d12be2ae0697b5f63af057be7d73a62194dd5
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-06-23 12:30:39 +0300

    go react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
+import { Component } from "react";
+import PokemonForm from "./PokemonForm";
+
+export default class App extends Component{
+
+  state = {
+    pokemon: null,
+    loading: false,
+  };
+
+  componentDidMount(){
+    this.setState({ loading: true });
+
+    setTimeout(()=>{
+      fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(res => res.json())
+      .then(pokemon => this.setState({pokemon}))
+      .finally(()=> this.setState({ loading: false}));
+    }, 1000)
+  }
+
+  render(){
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      {/* React homework template */}
+
+    <PokemonForm/>  
+
+    <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20}}>
+      {this.state.loading && <h1>Загружаем...</h1>}
+      {this.state.pokemon && <div>{this.state.pokemon.name}</div>}
+
+    </div>
+
+
+
+
     </div>
   );
+}
+
 };
---
+++ src/components/PokemonForm.jsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import { ImSearch } from 'react-icons/im';
+import { toast } from 'react-toastify';
+
+const styles = { form: { marginBottom: 20 }}
+
+export default class PokemonForm extends Component{
+    state = {
+        pokemon: '',
+    }
+
+    handleSubmit = event => {
+        if(this.state.pokemon.trim() === ''){
+            return toast.error('Введите имя покемона!')
+        }
+
+        console.log(this.state.pokemon);
+        
+
+        this.props.onSubmit(this.state.pokemonName);
+        this.setState({ pokemonName: ''})
+    };
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit} style={styles.form}>
+                <input
+                type="text"
+                name="pokemonName"
+                value={this.state.pokemonName}
+                onChange={this.handleNameChange}
+                />
+                <button type="submit">
+
+                    <ImSearch style={{ marginRight: 8}}/>
+                Найти    
+
+                </button>
+            </form>
+        );
+    }
+};
